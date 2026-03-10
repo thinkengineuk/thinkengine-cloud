69c9f398936a5d8e464a2f77fbcf3b9e6b0ff30b
Ben Michaelis <ben@thinkengine.co>
2026-03-10 15:40:00 +0000

    File changes

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -431,18 +431,10 @@ const sidebarMenuButtonVariants = cva(
   }
 )
 
-const SidebarMenuButton = React.forwardRef((
-  {
-    asChild = false,
-    isActive = false,
-    variant = "default",
-    size = "default",
-    tooltip,
-    className,
-    ...props
-  },
+const SidebarMenuButton = React.forwardRef(function SidebarMenuButtonInner(
+  { asChild = false, isActive = false, variant = "default", size = "default", tooltip, className, ...props },
   ref
-) => {
+) {
   const Comp = asChild ? Slot : "button"
   const { isMobile, state } = useSidebar()
 
