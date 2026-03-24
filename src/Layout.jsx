f3db5d2ab71c47e667bceece7fc889d59b499559
Ben Michaelis <ben@thinkengine.co>
2026-03-24 06:31:30 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { createPageUrl } from "@/utils";
-import { LayoutDashboard, User, LogOut, Upload, FileJson, BarChart2 } from "lucide-react";
+import { LayoutDashboard, User, LogOut, BarChart2 } from "lucide-react";
 import { User as UserEntity } from "@/entities/User";
 import { Board } from "@/entities/Board";
 import {
