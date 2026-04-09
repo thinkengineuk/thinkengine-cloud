31a5686d35ddc318c93459908a12552116c2098f
Ben Michaelis <ben@thinkengine.co>
2026-04-09 08:08:59 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+    safelist: [
+      "bg-blue-500", "bg-pink-500", "bg-red-500", "bg-orange-500",
+      "bg-green-500", "bg-purple-500", "bg-yellow-500", "bg-teal-500",
+      "bg-indigo-500", "bg-cyan-500"
+    ],
   theme: {
   	extend: {
   		borderRadius: {
